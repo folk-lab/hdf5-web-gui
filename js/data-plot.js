359ed794cf9edf17a6770239f2346a6c27cb7ae1
Jason Brudvik <Jason.Brudvik@maxlab.lu.se>
2016-12-31 13:53:07 +0100

    good 2d and 3d plots, profile histograms match zoomed area of 2d plots, log plots work pretty good

--- js/data-plot.js
+++ js/data-plot.js
@@ -1,17 +1,53 @@
-/*global Plotly*/
+/*global $*/
 'use strict';
 
-function drawVisualization(value, divName, plotType) {
+// Global variables
+var DATA_PLOT = {
+
+    plotCanvasDiv : document.getElementById('plotCanvasDiv'),
+    colorScale : 'Portland',
+    plotLogValues : false,
+    plotType : 'heatmap',
+    initialDataValues : [],
+    logOfDataValues : [],
+    dataValues : [],
+
+},
+
+    // External libraries
+    Plotly;
+
+
+function draw3DPlot() {
 
     var data, layout;
 
-    // Create data objects
+    // data = [
+    //     {
+    //         z: DATA_PLOT.dataValues,
+    //         type: plotType,
+    //         colorscale: [
+    //             [0, 'rgb(250, 250, 250)'],
+    //             [1.0 / 10000, 'rgb(200, 200, 200)'],
+    //             [1.0 / 1000, 'rgb(150, 150, 150)'],
+    //             [1.0 / 100, 'rgb(100, 100, 100)'],
+    //             [1.0 / 10, 'rgb(50, 50, 50)'],
+    //             [1.0, 'rgb(0, 0, 0)'],
+    //         ],
+    //         colorbar: {
+    //             tick0: 0,
+    //             tickmode: 'array',
+    //             tickvals: [0, 1000, 10000, 100000]
+    //         }
+    //     }
+    // ];
+
+    // Create data object
     data = [
         {
-            z: value,
-            type: plotType,
-            colorscale: 'Hot',
-            reversescale: true,
+            z: DATA_PLOT.dataValues,
+            type: DATA_PLOT.plotType,
+            colorscale: DATA_PLOT.colorScale,
         }
     ];
 
@@ -19,58 +55,130 @@ function drawVisualization(value, divName, plotType) {
     layout = {
         title: 'AgBehenate_228',
         autosize: false,
-        width: 600,
-        height: 500,
+        width: 550,
+        height: 550,
         margin: {
             l: 65,
             r: 50,
             b: 65,
             t: 90,
         },
+        scene: {
+            xaxis: {
+                title: 'x',
+            },
+            // zaxis: {
+            //     title: 'z blah',
+            //     type: 'log',
+            //     autorange: true
+            // }
+        }
     };
 
     // Present them
-    Plotly.newPlot(divName, data, layout);
+    Plotly.newPlot(DATA_PLOT.plotCanvasDiv, data, layout);
 
 }
 
 
-function drawFancyShit(value, divName, plotType) {
+function fillProfileHistograms(useFillLimitsX, xMin, xMax,
+    useFillLimitsY, yMin, yMax) {
 
-    var i, j, x1, y1, x2, y2,
-        xProfilePlot, yProfilePlot, data, layout, mainDataPlot;
+    var debug = true, i, j, useFillLimits = false, histValuesX1 = [],
+        histValuesY1 = [], histValuesX2 = [], histValuesY2 = [];
 
-    x1 = [];
-    y1 = [];
-    x2 = [];
-    y2 = [];
+    if (debug) {
+        console.log('useFillLimitsX: ' + useFillLimitsX);
+        console.log('xMin:           ' + xMin);
+        console.log('xMax:           ' + xMax);
+        console.log('useFillLimitsY: ' + useFillLimitsY);
+        console.log('yMin:           ' + yMin);
+        console.log('yMax:           ' + yMax);
+    }
 
-    console.log('value.length: ' + value.length);
-    for (i = 0; i < value.length; i += 1) {
+    if (useFillLimitsY || useFillLimitsX) {
+        useFillLimits = true;
+    }
 
-        x1[i] = i;
-        y1[i] = 0;
+    if (!useFillLimitsX) {
+        xMin = 0;
+        xMax = DATA_PLOT.dataValues[0].length;
+    }
 
-        for (j = 0; j < value[i].length; j += 1) {
+    if (!useFillLimitsY) {
+        yMin = 0;
+        yMax = DATA_PLOT.dataValues.length;
+    }
+
+    // Fill profile histograms
+    for (i = 0; i < DATA_PLOT.dataValues.length; i += 1) {
+
+        // The y-profile values
+        histValuesX1[i] = i;
+        histValuesY1[i] = 0;
+
+        for (j = 0; j < DATA_PLOT.dataValues[i].length; j += 1) {
 
             if (i === 0) {
-                x2[j] = j;
-                y2[j] = 0;
+                // The x-profile values
+                histValuesX2[j] = j;
+                histValuesY2[j] = 0;
             }
 
-            y1[i] += value[i][j];
-            y2[j] += value[i][j];
+            // If zooming, fill only relevant data
+            if (!useFillLimits || (useFillLimits &&
+                    i >= yMin && i < yMax && j >= xMin && j < xMax)) {
+                histValuesY1[i] += DATA_PLOT.dataValues[i][j];
+                histValuesY2[j] += DATA_PLOT.dataValues[i][j];
+            }
         }
     }
 
-    console.log('x1.length: ' + x1.length);
-    console.log('y1.length: ' + y1.length);
-    console.log('x1[0] ' + x1[0]);
+    if (debug) {
+        console.log('histValuesX1.length: ' + histValuesX1.length);
+        console.log('histValuesY1.length: ' + histValuesY1.length);
+        console.log('histValuesY1[' + yMin + ']:     ' + histValuesY1[yMin]);
+        console.log('histValuesX2.length: ' + histValuesX2.length);
+        console.log('histValuesY2.length: ' + histValuesY2.length);
+        console.log('histValuesY2[' + xMin + ']:     ' + histValuesY2[xMin]);
+    }
+
+    return {
+        histValuesX1 : histValuesX1,
+        histValuesY1 : histValuesY1,
+        histValuesX2 : histValuesX2,
+        histValuesY2 : histValuesY2,
+    };
+
+}
+
+
+function draw2DPlot() {
+
+    var profiles, xProfilePlot, yProfilePlot, data, layout, mainDataPlot;
+
+    console.log('DATA_PLOT.dataValues.length: ' + DATA_PLOT.dataValues.length);
+
+    profiles = fillProfileHistograms(false, 0, 0, false, 0, 0);
+
+    // The primary, 2-dimensional plot of the data - works best as a 'contour'
+    // plot me thinks
+    mainDataPlot = {
+        z: DATA_PLOT.dataValues,
+        // zmin: 2,
+        // zmax: 4,
+        zsmooth: 'best',
+        type: DATA_PLOT.plotType,
+        // line: {
+        //     smoothing: 0.5
+        // },
+        colorscale: DATA_PLOT.colorScale,
+    };
 
     // The x-profile of the plot, displayed as a bar chart / histogram
     xProfilePlot = {
-        x: x2,
-        y: y2,
+        x: profiles.histValuesX2,
+        y: profiles.histValuesY2,
         name: 'x profile',
         marker: {color: 'rgb(102,0,0)'},
         yaxis: 'y2',
@@ -80,22 +188,13 @@ function drawFancyShit(value, divName, plotType) {
     // The y-profile, also displayed as a bar chart / histogram, oriented
     // horizontally
     yProfilePlot = {
-        x: y1,
-        y: x1,
+        x: profiles.histValuesY1,
+        y: profiles.histValuesX1,
         name: 'y profile',
         marker: {color: 'rgb(102,0,0)'},
         xaxis: 'x2',
         type: 'bar',
         orientation: 'h'
-    };
-
-    // The primary, 2-dimensional plot of the data - works best as a 'contour'
-    // plot me thinks
-    mainDataPlot = {
-        z: value,
-        type: plotType,
-        colorscale: 'Hot',
-        reversescale: true,
     };
 
     // All the data that is to be plotted
@@ -108,18 +207,20 @@ function drawFancyShit(value, divName, plotType) {
         title: 'AgBehenate_228',
         showlegend: false,
         autosize: false,
-        width: 600,
-        height: 500,
+        width: 550,
+        height: 550,
         hovermode: 'closest',
         bargap: 0,
 
         xaxis: {
+            title: 'x',
             domain: [0, 0.85],
             showgrid: false,
             zeroline: false
         },
 
         yaxis: {
+            title: 'y',
             domain: [0, 0.85],
             showgrid: false,
             zeroline: false
@@ -135,15 +236,165 @@ function drawFancyShit(value, divName, plotType) {
             domain: [0.85, 1],
             showgrid: false,
             zeroline: false
-        }
+        },
     };
 
-    Plotly.newPlot(divName, data, layout);
+    Plotly.newPlot(DATA_PLOT.plotCanvasDiv, data, layout);
+
+
+    // Refill the profile histograms when a zoom event occurs
+    // Why isn't this done already in the plotly library?!
+    DATA_PLOT.plotCanvasDiv.on('plotly_relayout', function (eventdata) {
+
+        var useFillLimitsX = false, xMin = 0, xMax = 0,
+            useFillLimitsY = false, yMin = 0, yMax = 0;
+
+        console.log('plotly_relayout ' + DATA_PLOT.plotType);
+
+        console.log(JSON.stringify(eventdata));
+
+        if (eventdata.hasOwnProperty('xaxis.range[0]') &&
+                eventdata.hasOwnProperty('xaxis.range[1]')) {
+            xMin = Math.floor(eventdata['xaxis.range[0]']);
+            xMax = Math.ceil(eventdata['xaxis.range[1]']);
+            useFillLimitsX = true;
+        }
+        if (eventdata.hasOwnProperty('yaxis.range[0]') &&
+                eventdata.hasOwnProperty('yaxis.range[1]')) {
+            yMin = Math.floor(eventdata['yaxis.range[0]']);
+            yMax = Math.ceil(eventdata['yaxis.range[1]']);
+            useFillLimitsY = true;
+        }
+
+        console.log('x-axis start: ' + xMin);
+        console.log('x-axis end:   ' + xMax);
+        console.log('y-axis start: ' + yMin);
+        console.log('y-axis end:   ' + xMax);
+
+        profiles = fillProfileHistograms(useFillLimitsX, xMin, xMax,
+            useFillLimitsY, yMin, yMax);
+
+        Plotly.restyle(DATA_PLOT.plotCanvasDiv, {
+            x: [profiles.histValuesX2],
+            y: [profiles.histValuesY2],
+        }, [1]);
+
+        Plotly.restyle(DATA_PLOT.plotCanvasDiv, {
+            x: [profiles.histValuesY1],
+            y: [profiles.histValuesX1],
+        }, [2]);
+    });
+}
+
+
+function plotData() {
+
+    if (DATA_PLOT.plotType === 'surface') {
+        draw3DPlot(DATA_PLOT.plotType);
+    } else {
+        draw2DPlot(DATA_PLOT.plotType);
+    }
+}
+
+
+function changeType(type) {
+
+    ///////////////////////////////////////////////////////////////////////////
+    // Should use restyle instead of redrawing the entire plot:
+    //   https://plot.ly/javascript/plotlyjs-function-reference/#plotly-restyle
+    //
+    // This doesn't work so well currently, but maybe if I fix things up a bit?
+    // Plotly.restyle(DATA_PLOT.plotCanvasDiv, {
+    //     type: [type],
+    // }, [0]);
+    ///////////////////////////////////////////////////////////////////////////
+
+    if (type !== '') {
+        DATA_PLOT.plotType = type;
+        plotData();
+    }
 
 }
 
 
-function displayData(value) {
-    drawVisualization(value, 'myDiv1', 'surface');
-    drawFancyShit(value, 'myDiv2', 'contour');
+function changeColor(colorscale) {
+
+    if (colorscale !== '') {
+        DATA_PLOT.colorScale = colorscale;
+
+        Plotly.restyle(DATA_PLOT.plotCanvasDiv, {
+            colorscale: DATA_PLOT.colorScale
+        }, [0]);
+    }
+}
+
+
+function toggleLogPlot() {
+
+    DATA_PLOT.plotLogValues = !DATA_PLOT.plotLogValues;
+
+    if (DATA_PLOT.plotLogValues) {
+        $("#logPlotButton").html('Log Plot!');
+        $("#logPlotButton").addClass('btn-success');
+
+        DATA_PLOT.dataValues = DATA_PLOT.logOfDataValues;
+    } else {
+        $("#logPlotButton").html('Log Plot?');
+        $("#logPlotButton").removeClass('btn-success');
+
+        DATA_PLOT.dataValues = DATA_PLOT.initialDataValues;
+    }
+
+    // I can't get this restyle command to work quite right yet with log scales
+    // & colorscheme... the scale of the colorbar is all off :(
+    // Plotly.restyle(DATA_PLOT.plotCanvasDiv, {
+    //     z: [DATA_PLOT.dataValues],
+    // }, [0]);
+
+    plotData();
+}
+
+
+function initializeData(value) {
+    // Example for switching between different plotting types:
+    //  https://plot.ly/python/custom-buttons/
+
+    var i, j, logOfValue = [];
+
+    DATA_PLOT.initialDataValues = value;
+
+    // Take the log of the points, save for future use - is there a better
+    // way??
+    for (i = 0; i < value.length; i += 1) {
+        logOfValue[i] = [];
+        for (j = 0; j < value[i].length; j += 1) {
+            if (value[i][j] > 0) {
+                // Interesting - log10() is much slow than log()
+                //
+                // Also, this is slow:
+                //    value[i][j] = Math.log(value[i][j]) / Math.LN10;
+                // And just dividing by a number that equals Math.log(10) makes
+                // using the array slow...:
+                //    value[i][j] = Math.log(value[i][j]) / mathLog10;
+
+                // This seems to be the fastest - especially with contour plots
+                // Maybe don't use contour plots??
+                // logOfValue[i][j] = Math.log(value[i][j]);
+                // But maybe log10 makes more sense to use?
+                logOfValue[i][j] = Math.log(value[i][j]) / Math.LN10;
+            } else {
+                logOfValue[i][j] = 0;
+            }
+        }
+    }
+
+    // Save the log values
+    DATA_PLOT.logOfDataValues = logOfValue;
+
+    // Set the default data to use for plotting - raw values or thei log
+    if (DATA_PLOT.plotLogValues) {
+        DATA_PLOT.dataValues = DATA_PLOT.logOfDataValues;
+    } else {
+        DATA_PLOT.dataValues = DATA_PLOT.initialDataValues;
+    }
 }
--- js/data-retrieval.js
+++ js/data-retrieval.js
@@ -1,13 +1,14 @@
-/*global $, displayData*/
+/*global $, initializeData*/
 'use strict';
 
 
 // The gloabl variables for this applicaiton
 var DATA_RET =
     {
+        // h5serv has an issue with full hostnames
         hdf5DataServer: window.location.protocol + '//' +
-                        window.location.hostname +
-                        ':5000/datasets/',
+                        window.location.hostname.replace('.maxiv.lu.se', '') +
+                        ':5000',
     };
 
 
@@ -23,15 +24,19 @@ $.ajaxSetup({
 
 function getData(dataValueUrl) {
 
-    // Get the species data then update the table
+    // Get the data
     $.ajax({
         url: dataValueUrl,
 
         success: function (response) {
-            console.log("AJAX " + dataValueUrl + " request success");
+            var debug = false;
+
+            if (debug) {
+                console.log("AJAX " + dataValueUrl + " request success");
+            }
 
             if (response.hasOwnProperty('value')) {
-                displayData(response.value);
+                initializeData(response.value);
             }
         },
 
@@ -47,18 +52,14 @@ function getData(dataValueUrl) {
 // This function fires when the page is loaded
 $(document).ready(function () {
 
-    console.log('document is ready');
+    var debug = false;
 
-    // getData("http://localhost:5000/datasets/" +
-    //     "809a7a4c-a4f2-11e6-9d2c-080027343bb1/" +
-    //     "value?host=p1m_mask_mandelbrot-s02.from-zdenek.hdfgroup.org");
+    if (debug) {
+        console.log('document is ready');
+    }
 
-    // getData("http://localhost:5000/datasets/" +
-    //     "f9fa1489-baec-11e6-8791-080027343bb1/" +
-    //     "value?host=tall.public.hdfgroup.org");
-
-    getData(DATA_RET.hdf5DataServer +
-        "4b01d1b0-a4f2-11e6-9d2c-080027343bb1/" +
-        "value?host=AgBehenate_228.from-zdenek.hdfgroup.org");
+    getData(DATA_RET.hdf5DataServer + '/datasets/' +
+        "c64df0ee-c6a6-11e6-b2af-080027343bb1/" +
+        "value?host=AgBehenate_228.from-zdenek.maxiv.lu.se");
 
 });
